a79b767a37984adae8f0dfd9187603194327aab0
tktk8924 <myphone.tk@gmail.com>
2016-05-31 14:22:18 +0900

    circleci設定修正

--- examples/flaskr/deploy.sh
+++ examples/flaskr/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-docker push $REGISTRY
+registry=$1
 
-old_id=`cf ic ps | grep $REGISTRY | awk '{print $1}'`
+docker tag -f $registry $registry:$CIRCLE_SHA1
+docker push $registry:$CIRCLE_SHA1
+
+old_id=`cf ic ps | grep $registry | awk '{print $1}'`
 echo "Old Container ID: $old_id"
 
 public_ip=`cf ic inspect $old_id | jq -r '.[0].NetworkSettings.PublicIpAddress'`
@@ -12,7 +15,7 @@ echo "Unbind public IP from old container"
 cf ic ip unbind $public_ip $old_id
 
 echo "Run new container"
-new_id=`cf ic run -p 80 $REGISTRY`
+new_id=`cf ic run -p 80 $registry:$CIRCLE_SHA1`
 echo "New Container ID: $new_id"
 
 echo "Bind public IP to new container"
